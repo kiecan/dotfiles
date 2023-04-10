e68aa7d16a80cb2d10f587af42946f5df9acb932
Kieran Canavan <kiecanavan@gmail.com>
2023-04-10 11:14:13 +0100

    Updaing nvim config
    Signed-off-by: Kieran Canavan <kiecanavan@gmail.com>

---
+++ .config/nvim/lua/kiecan/core/options.lua
@@ -0,0 +1,39 @@
+local opt = vim.opt -- Shortcut for config lines
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- Tabs ann indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.hlsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- enable system keyboard
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-") -- Make '-' part of words
--- .config/nvim/lua/kiecan/init.lua
+++
@@ -1,1 +0,0 @@
-require("kiecan.remap")
