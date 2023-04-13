98351db3739f246d1cba90355bc67dc078ddf312
Kieran Canavan <kieran.canavan@typeform.com>
2023-04-13 12:13:42 +0200

    feat(nvim): adding nvim-tree plugin
    Signed-off-by: Kieran Canavan <kieran.canavan@typeform.com>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,6 +58,16 @@ require('lazy').setup({
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
   { -- Adds git releated signs to the gutter, as well as utilities for managing changes
